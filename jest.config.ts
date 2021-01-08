61ec86149d01e767e20eee46b31b43c92670e5e4
Lucas Santos <lucassm02@gmail.com>
2021-01-08 02:29:31 +0000

    chore: updates the jest configuration

--- jest.config.ts
+++ jest.config.ts
@@ -3,5 +3,5 @@ export default {
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
-  transform: { '.+\\.ts$': 'ts-node' }
+  transform: { '.+\\.ts$': 'ts-jest' }
 }
