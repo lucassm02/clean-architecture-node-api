218e57923e269c2a240ed8caf189fe5e5856ca51
Lucas Santos <lucassm02@gmail.com>
2021-02-03 23:48:19 +0000

    test: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/accountRepository/Account.spec.ts
@@ -0,0 +1,20 @@
+import { mongoHelper } from '../helpers/mongoHelper'
+import { AccountMongoRepository } from './Account'
+describe('Account Mongo repository', () => {
+  beforeAll(async () => await mongoHelper.connect(process.env.MONGO_URL))
+  afterAll(async () => await mongoHelper.disconnect())
+
+  test('should return an account on success', async () => {
+    const sut = new AccountMongoRepository()
+    const account = await sut.add({
+      name: 'Lucas',
+      email: 'lucas@gmail.com',
+      password: 'hash'
+    })
+
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('Lucas')
+    expect(account.email).toBe('lucas@gmail.com')
+  })
+})
