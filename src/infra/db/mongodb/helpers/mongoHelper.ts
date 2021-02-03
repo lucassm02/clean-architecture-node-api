6e2062acd231e30efe505d7690a851e22366a147
Lucas Santos <lucassm02@gmail.com>
2021-02-03 23:47:00 +0000

    feat: add method getCollection

--- src/infra/db/mongodb/helpers/mongoHelper.ts
+++ src/infra/db/mongodb/helpers/mongoHelper.ts
@@ -1,6 +1,6 @@
-import { MongoClient } from 'mongodb'
+import { Collection, MongoClient } from 'mongodb'
 
-export const MongoHelper = {
+export const mongoHelper = {
   client: null as MongoClient,
   async connect (url: string): Promise<void> {
     this.client = await MongoClient.connect(url, {
@@ -10,5 +10,8 @@ export const MongoHelper = {
   },
   async disconnect (): Promise<void> {
     await this.client.close()
+  },
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
   }
 }
