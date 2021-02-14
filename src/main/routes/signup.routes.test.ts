27ce91d262e976872346ec934d03d5e65bd06a59
Lucas Santos <lucassm02@gmail.com>
2021-02-14 23:14:45 +0000

    fix: fix mongodb connection issue

--- src/main/routes/signup.routes.test.ts
+++ src/main/routes/signup.routes.test.ts
@@ -3,16 +3,16 @@ import { mongoHelper } from '../../infra/db/mongodb/helpers/mongoHelper'
 import app from '../config/app'
 
 describe('SingUp Route', () => {
+  beforeAll(async () => await mongoHelper.connect(process.env.MONGO_URL))
+  afterAll(async () => await mongoHelper.disconnect())
+  beforeAll(async () => await mongoHelper.getCollection('accounts').deleteMany({}))
   test('should return an account on success', async () => {
-    beforeAll(async () => await mongoHelper.connect(process.env.MONGO_URL))
-    afterAll(async () => await mongoHelper.disconnect())
-    beforeAll(async () => await mongoHelper.getCollection('accounts').deleteMany({}))
     await request(app)
       .post('/api/signup').send({
-        nome: 'lucas',
-        email: 'lucas@gmail.com',
-        password: '123',
-        confirmation_password: '123'
+        name: 'Lucas',
+        email: 'lucastest@gmail.com',
+        password: '12345678',
+        password_confirmation: '12345678'
       }).expect(200)
   })
 })
