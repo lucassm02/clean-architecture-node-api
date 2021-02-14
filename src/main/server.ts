9bf26b078369691dab142ae32e8b3b86b01c8b59
Lucas Santos <lucassm02@gmail.com>
2021-02-14 21:28:03 +0000

    feat: add dotenv

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,6 +1,10 @@
 import { mongoHelper } from '../infra/db/mongodb/helpers/mongoHelper'
 
 import env from './config/env'
+import { config } from 'dotenv'
+import { resolve } from 'path'
+
+config({ path: resolve(__dirname, '..', '..', '.env') })
 
 mongoHelper.connect(env.mongoUrl).then(async () => {
   const { default: app } = (await import('./config/app'))
