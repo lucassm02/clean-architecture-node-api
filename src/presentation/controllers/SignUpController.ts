baa9caf2b2edae6d0ede8e35b5ec604d5035bf5d
Lucas Santos <lucassm02@gmail.com>
2021-02-21 17:41:27 +0000

    refactor: make server error return the error stack

--- src/presentation/controllers/SignUpController.ts
+++ src/presentation/controllers/SignUpController.ts
@@ -34,7 +34,7 @@ export default class SignUpController implements Controller {
 
       return ok(account)
     } catch (error) {
-      return serverError()
+      return serverError(error)
     }
   }
 }
